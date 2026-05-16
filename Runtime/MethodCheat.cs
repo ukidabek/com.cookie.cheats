77f7ac31cd27bc0a3f9c8f6f873bd18fa038cb79
Łukasz Dąbek <ukidabek@gmail.com>
2026-05-16 21:46:30 +0200

    [Code] Method parameters conversion fixed.

--- Runtime/MethodCheat.cs
+++ Runtime/MethodCheat.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Reflection;
 
 namespace cookie.Cheats
@@ -7,14 +8,23 @@ namespace cookie.Cheats
     public class MethodCheat : Cheat
     {
         private readonly MethodInfo m_methodInfo = null;
+        private readonly Type[] m_parameterTypes = null;
         
         public MethodCheat(int id, object target, MethodInfo memberInfo) : base(id, target, memberInfo)
         {
             m_methodInfo = memberInfo;
+            m_parameterTypes = memberInfo.GetParameters().Select(parameterInfo => parameterInfo.ParameterType).ToArray();
         }
 
         public void Invoke(object[] parameters)
         {
+            if (parameters != null && parameters.Any())
+            {
+                var lenght = parameters.Length;
+                for (var i = 0; i < lenght; i++)
+                    parameters[i] = Convert.ChangeType(parameters[i], m_parameterTypes[i]);
+            }
+
             m_methodInfo.Invoke(Target, parameters);
         }
 
